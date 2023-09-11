4e1ee7f917082c18f3ca167ddbd03da984abaf16
Pablo Fernando Mesquida <pablo.mesquida@ecam.es>
2023-09-11 14:43:34 +0200

    model4

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,68 @@
 import { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Stage } from '@react-three/drei'
 import { useControls } from 'leva'
 import { Perf } from 'r3f-perf'
 import Loader from './components/Loader.jsx'
-import Lights from './components/Lights.jsx'
+// import Lights from './components/Lights.jsx'
 import Model1 from './components/models/JarronBlender.jsx'
 import Model2 from './components/models/MesaBlender.jsx'
 import Model3 from './components/models/DioramaBlenderTextures.jsx'
 import Model4 from './components/models/Room-03.jsx'
 
-
 function App() {
   const { model, count } = useControls({
-    model: { value: 'Jarron', options: ['Jarron', 'Mesa', 'Room'] },
-    count: { value: 50, min: 1, max: 1000, step: 1 },
-  });
+    model: { value: 'Jarron', options: ['Jarron', 'Mesa', 'Room', 'Room3'] },
+    count: { value: 1, min: 1, max: 1000, step: 1 },
+  })
 
   const models = {
     Jarron: <Model1 />,
     Mesa: <Model2 />,
     Room: <Model3 />,
     Room3: <Model4 />,
-  };
+  }
+
+  const selectedModel = models[model] || null
 
   // Función para generar una posición en una cuadrícula con separación de 10 en x y z
   const getPosition = (index) => {
-    const separation = 25;
-    const perRow = Math.ceil(Math.sqrt(count));
-    const x = (index % perRow) * separation;
-    const z = Math.floor(index / perRow) * separation;
-    return [x, 0, z];
-  };
-
+    const separation = 25
+    const perRow = Math.ceil(Math.sqrt(count))
+    const x = (index % perRow) * separation
+    const z = Math.floor(index / perRow) * separation
+    return [x, 0, z]
+  }
 
   const renderModels = () => {
     return Array.from({ length: count }).map((_, index) => {
-      switch (model) {
+      switch (selectedModel) {
         case 'Jarron':
-          return <Model1 key={index} position={getPosition(index)} />;
+          return <Model1 key={index} position={getPosition(index)} />
         case 'Mesa':
-          return <Model2 key={index} position={getPosition(index)} />;
+          return <Model2 key={index} position={getPosition(index)} />
         case 'Room':
-          return <Model3 key={index} position={getPosition(index)} />;
+          return <Model3 key={index} position={getPosition(index)} />
+        case 'Room3':
+          return <Model4 key={index} position={getPosition(index)} />
         default:
-          return null;
+          return null
       }
-    });
+    })
   }
-  
 
   return (
-      <Canvas >
-        <Lights />
-        <Perf position="top-left" />
-        <OrbitControls makeDefault/> 
-        <Suspense fallback={<Loader />}>
-          {/* <Stage intensity={5} environment="forest" shadows="contact"> */}
-            {selectedModel}   
-          {/* </Stage> */}
-        </Suspense>
+    <Canvas>
+      {/*       <Lights /> */}
+      <Perf position='top-left' />
+      <OrbitControls makeDefault />
+      <Suspense fallback={<Loader />}>
+        <Stage intensity={5} environment='forest' shadows='contact'>
+          <Model4 />
+        </Stage>
+      </Suspense>
     </Canvas>
-  ) 
+  )
 }
 
-export default App;
+export default App
--- src/components/Lights.jsx
+++ src/components/Lights.jsx
@@ -1,20 +1,20 @@
-import { useRef } from 'react';
-import { useHelper } from '@react-three/drei'
-import { DirectionalLightHelper } from 'three';
+import { useRef } from 'react'
+// import { useHelper } from '@react-three/drei'
+// import { DirectionalLightHelper } from 'three'
 
-export default function Lights () {
-    const directionalLightRef = useRef();
+export default function Lights() {
+  const directionalLightRef = useRef()
 
-    useHelper(directionalLightRef, DirectionalLightHelper, 'red');
+  //   useHelper(directionalLightRef, DirectionalLightHelper, 'red');
 
-    return( <>     
-          <directionalLight
-            ref={directionalLightRef}
-            intensity={1}
-            position={[1, 5, 3]} 
-            castShadow 
-          />
-
-        </>
-    )
+  return (
+    <>
+      <directionalLight
+        // ref={directionalLightRef}
+        intensity={1}
+        position={[1, 5, 3]}
+        castShadow
+      />
+    </>
+  )
 }
